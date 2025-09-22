714ad8e0f054f3059051ea803c64eebd0f70ce9a
alexeps1 <alex.epskamp@massivemusic.com>
2025-09-22 16:21:41 +0200

    initial commit

---
+++ smiley.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+print("😊")
+print()
+print("Or using keyboard characters:")
+print(":)")
+print(":-)")
+print("=)")
+print()
+print("ASCII art version:")
+print("  ^_^")
+print(" (o o)")
+print(" (   )")
+print("  \\_/")
